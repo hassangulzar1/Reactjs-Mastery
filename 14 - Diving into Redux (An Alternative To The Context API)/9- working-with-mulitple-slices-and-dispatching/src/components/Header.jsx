05bcd6c2d5fd26588b8a86b4a1d464af0e00ff15
hassangulzar1 <hassangulzar121@gmail.com>
2023-09-29 15:38:47 +0500

    last section left

--- 14 - Diving into Redux (An Alternative To The Context API)/9- working-with-mulitple-slices-and-dispatching/src/components/Header.jsx
+++ 14 - Diving into Redux (An Alternative To The Context API)/9- working-with-mulitple-slices-and-dispatching/src/components/Header.jsx
@@ -1,8 +1,15 @@
 import classes from "./Header.module.css";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { authActions } from "../store";
 
 const Header = () => {
+  const dispatch = useDispatch();
   const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
+  const logOutHandler = (e) => {
+    e.preventDefault();
+    dispatch(authActions.logout());
+  };
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
@@ -16,7 +23,7 @@ const Header = () => {
               <a href="/">My Sales</a>
             </li>
             <li>
-              <button>Logout</button>
+              <button onClick={logOutHandler}>Logout</button>
             </li>
           </ul>
         </nav>
