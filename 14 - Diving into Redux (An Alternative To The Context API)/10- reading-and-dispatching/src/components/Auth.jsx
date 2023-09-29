251d23077901ce5f05c39e8f3bfa960ae8ea133a
hassangulzar1 <hassangulzar121@gmail.com>
2023-09-29 15:53:21 +0500

    splitting our code

--- 14 - Diving into Redux (An Alternative To The Context API)/10- reading-and-dispatching/src/components/Auth.jsx
+++ 14 - Diving into Redux (An Alternative To The Context API)/10- reading-and-dispatching/src/components/Auth.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import classes from "./Auth.module.css";
-import { authActions } from "../store";
+import { authActions } from "../store/auth-slice";
 const Auth = () => {
   const [inputState, setInputState] = useState({ email: "", password: "" });
   const inputChangeHandler = (e) => {
