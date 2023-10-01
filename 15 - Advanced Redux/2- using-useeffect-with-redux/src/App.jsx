e0b98a144646c107cc6495158e071f07843e5dff
hassangulzar1 <hassangulzar121@gmail.com>
2023-10-02 00:08:09 +0500

    using useeffect with redux complete

--- 15 - Advanced Redux/2- using-useeffect-with-redux/src/App.jsx
+++ 15 - Advanced Redux/2- using-useeffect-with-redux/src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
@@ -5,8 +6,13 @@ import { useSelector } from "react-redux";
 
 function App() {
   const counterToggle = useSelector((state) => state.ui.cartIsVisible);
-  const cart = useSelector((state) => state.ui.cart);
-  console.log(cart);
+  const cart = useSelector((state) => state.cart);
+  useEffect(() => {
+    fetch("https://react-http-f062f-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
   return (
     <Layout>
       {counterToggle && <Cart />}
