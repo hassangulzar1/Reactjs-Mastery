9e3be50e21307fdb03de8aef9b1de91c75d08d5e
hassangulzar1 <hassangulzar121@gmail.com>
2023-10-03 00:04:47 +0500

    http error done

--- 15 - Advanced Redux/3- handling-http-state-feedback-with-redux/src/App.jsx
+++ 15 - Advanced Redux/3- handling-http-state-feedback-with-redux/src/App.jsx
@@ -1,10 +1,11 @@
 import { Fragment, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { uiActions } from "./store/ui-slice";
 import Notification from "./components/UI/Notification";
-import { sendCartData } from "./store/cart-slice";
 
 let isInitial = true;
 
@@ -15,13 +16,49 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    console.log("use effect call");
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data!",
+        })
+      );
+      const response = await fetch(
+        "https://react-http-f062f-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed.");
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    };
+
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    dispatch(sendCartData(cart));
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed!",
+        })
+      );
+    });
   }, [cart, dispatch]);
 
   return (
