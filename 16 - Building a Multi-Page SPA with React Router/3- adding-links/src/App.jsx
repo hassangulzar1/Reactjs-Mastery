7f4cb721c10506672dc2408fc31e7cc1a9fd29d3
hassangulzar1 <hassangulzar121@gmail.com>
2023-10-03 23:26:35 +0500

    adding link completed

--- 16 - Building a Multi-Page SPA with React Router/3- adding-links/src/App.jsx
+++ 16 - Building a Multi-Page SPA with React Router/3- adding-links/src/App.jsx
@@ -1,24 +1,11 @@
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
-  Route,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
 
-const routeDefinitions = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<Home />} />
-    <Route path="/products" element={<Product />} />
-  </Route>
-);
-// const router = createBrowserRouter([
-//   { path: "/", element: <Home /> },
-//   { path: "/products", element: <Product /> },
-// ]);
-
-const router = createBrowserRouter(routeDefinitions);
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/products", element: <Product /> },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
