8a2efde6c8d603b1bb2e7806cba83c2542a90014
hassangulzar1 <hassangulzar121@gmail.com>
2023-10-03 23:46:44 +0500

    error route completed

--- 16 - Building a Multi-Page SPA with React Router/5- error-route-404/src/App.jsx
+++ 16 - Building a Multi-Page SPA with React Router/5- error-route-404/src/App.jsx
@@ -2,11 +2,12 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
 import Root from "./pages/Root";
-
+import Error from "./pages/Error";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <Error />,
     children: [
       { path: "/", element: <Home /> },
       { path: "/products", element: <Product /> },
