49851a1acef90b0a97e82b2a3c61693bfaa6da28
hassangulzar1 <hassangulzar121@gmail.com>
2023-10-07 22:49:21 +0500

    use route loader data

---
+++ 16 - Building a Multi-Page SPA with React Router/16- use-route-loader-data/frontend/src/pages/EventDetailPage.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link, json, useRouteLoaderData } from "react-router-dom";
+import EventItem from "../components/EventItem";
+const EventDetailPage = () => {
+  const data = useRouteLoaderData("event-detail");
+
+  return (
+    <>
+      <EventItem event={data.event} />
+      <p>
+        <Link to=".." relative="path">
+          Back
+        </Link>
+      </p>
+    </>
+  );
+};
+
+export default EventDetailPage;
+
+export async function loader({ request, params }) {
+  const id = params.eventId;
+  const response = await fetch("http://localhost:8080/events/" + id);
+  if (!response.ok) {
+    throw json(
+      { message: "Could not fetch details for selected event." },
+      {
+        status: 500,
+      }
+    );
+  } else {
+    return response;
+  }
+}
