cee29e16ca014157e357ae546aba66168c969cfe
hassangulzar1 <hassangulzar121@gmail.com>
2023-10-07 21:55:52 +0500

    extracting error from data

--- 16 - Building a Multi-Page SPA with React Router/14- handling-errors/frontend/src/pages/EventsPage.jsx
+++ 16 - Building a Multi-Page SPA with React Router/14- handling-errors/frontend/src/pages/EventsPage.jsx
@@ -19,8 +19,14 @@ export const loader = async () => {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
+    //! 1st Method
     // return { isError: true, message: "Could not fetch event" };
-    throw { message: "Could not fetch event " };
+    //! 2nd Method
+    // throw { message: "Could not fetch event " };
+    //! 3rd Method
+    throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+      status: 500,
+    });
   } else {
     return response;
 
