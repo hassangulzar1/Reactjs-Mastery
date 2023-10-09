fdd272dde9aa0340378465cccd611b3ddb6fbfa7
hassangulzar1 <hassangulzar121@gmail.com>
2023-10-09 19:02:04 +0500

    module completed

--- 16 - Building a Multi-Page SPA with React Router/24- control which data should be deffered/frontend/src/pages/EventDetailPage.jsx
+++ 16 - Building a Multi-Page SPA with React Router/24- control which data should be deffered/frontend/src/pages/EventDetailPage.jsx
@@ -1,20 +1,36 @@
-import React from "react";
-import { Link, json, redirect, useRouteLoaderData } from "react-router-dom";
+import React, { Suspense } from "react";
+import {
+  Link,
+  defer,
+  json,
+  redirect,
+  useRouteLoaderData,
+  Await,
+} from "react-router-dom";
 import EventItem from "../components/EventItem";
+import EventsList from "../components/EventsList";
 const EventDetailPage = () => {
-  const data = useRouteLoaderData("event-detail");
+  const { event, events } = useRouteLoaderData("event-detail");
 
   return (
     <>
-      <EventItem event={data.event} />
+      <Suspense fallback={<p style={{ textAlign: "center" }}>Loading....</p>}>
+        <Await resolve={event}>
+          {(loadedEvent) => <EventItem event={loadedEvent} />}
+        </Await>
+      </Suspense>
+      <Suspense fallback={<p style={{ textAlign: "center" }}>Loading....</p>}>
+        <Await resolve={events}>
+          {(loadedEvents) => <EventsList events={loadedEvents} />}
+        </Await>
+      </Suspense>
     </>
   );
 };
 
 export default EventDetailPage;
 
-export async function loader({ request, params }) {
-  const id = params.eventId;
+async function loadEvent(id) {
   const response = await fetch("http://localhost:8080/events/" + id);
   if (!response.ok) {
     throw json(
@@ -24,8 +40,32 @@ export async function loader({ request, params }) {
       }
     );
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.event;
   }
+}
+async function loadEvents() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    return json(
+      { message: "could not fetch events." },
+      {
+        status: 500,
+      }
+    );
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+}
+
+export async function loader({ request, params }) {
+  const id = params.eventId;
+  return defer({
+    event: await loadEvent(id),
+    events: loadEvents(),
+  });
 }
 
 export async function action({ params, request }) {
--- 16 - Building a Multi-Page SPA with React Router/24- control which data should be deffered/frontend/src/pages/HomePage.jsx
+++ 16 - Building a Multi-Page SPA with React Router/24- control which data should be deffered/frontend/src/pages/HomePage.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-
+import PageContent from "../components/PageContent";
 const HomePage = () => {
-  return <h1>HomePage</h1>;
+  return (
+    <PageContent title="Welcome">Browse all our amazing events!</PageContent>
+  );
 };
 
 export default HomePage;
