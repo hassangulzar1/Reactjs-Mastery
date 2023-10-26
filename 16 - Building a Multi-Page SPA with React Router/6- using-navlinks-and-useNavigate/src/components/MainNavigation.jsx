87645834f6d4691731d8cd4288367e84e604a45f
hassangulzar1 <hassangulzar121@gmail.com>
2023-10-26 22:41:17 +0500

    creating next.js folder

--- 16 - Building a Multi-Page SPA with React Router/6- using-navlinks-and-useNavigate/src/components/MainNavigation.jsx
+++ 16 - Building a Multi-Page SPA with React Router/6- using-navlinks-and-useNavigate/src/components/MainNavigation.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import classes from "./MainNavigation.module.css";
+
 const MainNavigation = () => {
   return (
     <header className={classes.header}>
