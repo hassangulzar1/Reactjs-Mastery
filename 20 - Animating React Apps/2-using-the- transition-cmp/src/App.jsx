63b6eec11e7eecf54c5a6eedef17b8934c1a34a6
hassangulzar1 <hassangulzar121@gmail.com>
2023-11-29 16:19:37 +0500

    woring on animation

--- 20 - Animating React Apps/2-using-the- transition-cmp/src/App.jsx
+++ 20 - Animating React Apps/2-using-the- transition-cmp/src/App.jsx
@@ -4,9 +4,12 @@ import "./App.css";
 import List from "./components/List/List";
 import Backdrop from "./components/Backdrop/Backdrop";
 import Modal from "./components/Modal/Modal";
+import Transition from "react-transition-group/Transition";
+
 class App extends Component {
   state = {
     modalIsOpen: false,
+    showBlock: false,
   };
   showModal = () => {
     this.setState({ modalIsOpen: true });
@@ -19,12 +22,38 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        {this.state.modalIsOpen ? (
-          <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
-        ) : null}
-        {this.state.modalIsOpen ? (
-          <Backdrop show={this.state.modalIsOpen} />
-        ) : null}
+        <button
+          className="Button"
+          onClick={() =>
+            this.setState((prevState) => ({ showBlock: !prevState.showBlock }))
+          }
+        >
+          Toggle
+        </button>
+        <br />
+        <Transition
+          in={this.state.showBlock}
+          timeout={1000}
+          mountOnEnter
+          unmountOnExit
+        >
+          {(state) => (
+            <div
+              style={{
+                backgroundColor: "red",
+                width: 100,
+                height: 100,
+                margin: "auto",
+                transition: "opacity 1s ease-out",
+                opacity: state === "exiting" ? 0 : 1,
+              }}
+            ></div>
+          )}
+        </Transition>
+
+        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
+
+        {this.state.modalIsOpen ? <Backdrop show /> : null}
         <button className="Button" onClick={this.showModal}>
           Open Modal
         </button>
