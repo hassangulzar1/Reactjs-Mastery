6a5b4715deb0db428c3196089c61f34a7ed95317
hassangulzar1 <hassangulzar121@gmail.com>
2023-11-29 16:37:35 +0500

    customizing css class names

--- 20 - Animating React Apps/3-transition-events/src/components/Modal/Modal.jsx
+++ 20 - Animating React Apps/3-transition-events/src/components/Modal/Modal.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Transition from "react-transition-group/Transition";
+import CSSTransition from "react-transition-group/CSSTransition";
 import "./Modal.css";
 
 const animationTiming = {
@@ -9,37 +9,26 @@ const animationTiming = {
 
 const Modal = (props) => {
   return (
-    <Transition
+    <CSSTransition
       mountOnEnter
       unmountOnExit
       in={props.show}
       timeout={animationTiming}
-      onEnter={() => console.log("onEnter")}
-      onEntering={() => console.log("onEntering")}
-      onEntered={() => console.log("onEntered")}
-      onExit={() => console.log("onExit")}
-      onExiting={() => console.log("onExiting")}
-      onExited={() => console.log("onExited")}
+      classNames="fade-slide"
+      // onEnter={() => console.log("onEnter")}
+      // onEntering={() => console.log("onEntering")}
+      // onEntered={() => console.log("onEntered")}
+      // onExit={() => console.log("onExit")}
+      // onExiting={() => console.log("onExiting")}
+      // onExited={() => console.log("onExited")}
     >
-      {(state) => {
-        const cssClasses = [
-          "Modal",
-          state == "entering"
-            ? "ModalOpen"
-            : state == "exiting"
-            ? "ModalClosed"
-            : null,
-        ];
-        return (
-          <div className={cssClasses.join(" ")}>
-            <h1>A Modal</h1>
-            <button className="Button" onClick={props.closed}>
-              Dismiss
-            </button>
-          </div>
-        );
-      }}
-    </Transition>
+      <div className="Modal">
+        <h1>A Modal</h1>
+        <button className="Button" onClick={props.closed}>
+          Dismiss
+        </button>
+      </div>
+    </CSSTransition>
   );
 };
 
