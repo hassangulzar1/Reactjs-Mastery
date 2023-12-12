00302eec43c05a7cc9950c5f6af0563ced20578b
hassangulzar1 <hassangulzar121@gmail.com>
2023-12-12 21:26:15 +0500

    learning typeScript

---
+++ 22 - React + TypeScript/1-intoducing-basic-types/basics.ts
@@ -0,0 +1,14 @@
+// primitives: number, string, boolean
+// More complex types: arrays, objects
+// Fuunction types, parameters
+
+//Primitives
+
+let age: number;
+age = 12.1;
+
+let userName: string;
+userName = "Hassan";
+
+let isInstructor: boolean;
+isInstructor = true;
