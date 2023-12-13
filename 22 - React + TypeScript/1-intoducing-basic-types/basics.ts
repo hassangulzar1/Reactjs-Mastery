f77f8a2e6a7790d16903a4500b85c7792c33b48e
hassangulzar1 <hassangulzar121@gmail.com>
2023-12-13 15:01:22 +0500

    working

--- 22 - React + TypeScript/1-intoducing-basic-types/basics.ts
+++ 22 - React + TypeScript/1-intoducing-basic-types/basics.ts
@@ -12,3 +12,31 @@ userName = "Hassan";
 
 let isInstructor: boolean;
 isInstructor = true;
+
+// More complex types
+
+let hobbie: string[];
+hobbie = ["Sports", "Cooking"];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Max",
+  age: 32,
+};
+
+// person = {
+//   isEmployee: true,
+// };
+
+let people: {
+  name: string;
+  age: number;
+}[];
+
+// Type inference
+
+let course = "React - The complete Guide";
