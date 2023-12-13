b084cb199ed0f6fbd13b2227aa5c1fc07ba328bb
hassangulzar1 <hassangulzar121@gmail.com>
2023-12-13 15:07:20 +0500

    working on ts

---
+++ 22 - React + TypeScript/2-using-union-types/basics.ts
@@ -0,0 +1,42 @@
+// primitives: number, string, boolean
+// More complex types: arrays, objects
+// Fuunction types, parameters
+
+//Primitives
+
+let age: number;
+age = 12.1;
+
+let userName: string | string[];
+userName = "Hassan";
+
+let isInstructor: boolean;
+isInstructor = true;
+
+// More complex types
+
+let hobbie: string[];
+hobbie = ["Sports", "Cooking"];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Max",
+  age: 32,
+};
+
+// person = {
+//   isEmployee: true,
+// };
+
+let people: {
+  name: string;
+  age: number;
+}[];
+
+// Type inference
+let course: string | number = "React - The complete Guide";
+course = 12121;
